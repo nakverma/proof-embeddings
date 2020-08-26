289972fc1cd420cc1da3286d202d713bbe3fb2c8
Uzay Macar <oum2000@columbia.edu>
2020-08-27 00:37:25 +0300

    Added i) step annotation, ii) auto-complete, and iii) step-by-step error messaging for logic-questioner.

--- logic-questioner/main.py
+++ logic-questioner/main.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from wtforms import Form, StringField, validators, FormField, FieldList, Label, TextAreaField, SelectField
-from wtforms.validators import StopValidation, DataRequired
+from wtforms.validators import StopValidation, ValidationError, InputRequired
 from flask_wtf import FlaskForm
 import random
 from wtforms.compat import string_types, text_type
@@ -18,74 +18,26 @@ questions = ["Prove that (p∨q)∨(p∨¬q) is a tautology.",
              "Prove that ((p→q)∧(q→r))→(p→r) is a tautology."]
 random_question = random.choice(questions)
 start_tokens = ['(', '~', '¬', 'p', 'q', 'r']
+laws = ['', 'Identity', 'Tautology', 'Associativity', 'Commutativity', 'Logical Equivalence', 'DeMorgan', 'Fallacy']
 
 
-class InputRequired(object):
-    """
-    Validates that input was provided for this field.
-
-    Note there is a distinction between this and DataRequired in that
-    InputRequired looks that form-input data was provided, and DataRequired
-    looks at the post-coercion data.
-    """
-    field_flags = ('required', )
-
-    def __init__(self, message=None):
-        self.message = message
-
-    def __call__(self, form, field):
-        if not field.raw_data or not field.raw_data[0]:
-            if self.message is None:
-                message = field.gettext('This field is required.')
-            else:
-                message = self.message
-
-            field.errors[:] = []
-            raise StopValidation(message)
+def step_input_check(step):
+    if len(step.data['step'].strip()) == 0:
+        return False
+    return True
 
 
-class DataRequired(object):
-    """
-    Checks the field's data is 'truthy' otherwise stops the validation chain.
+def step_syntax_check(step):
+    if not checkSyntax(step.data['step']):
+        return False
+    return True
 
-    This validator checks that the ``data`` attribute on the field is a 'true'
-    value (effectively, it does ``if field.data``.) Furthermore, if the data
-    is a string type, a string containing only whitespace characters is
-    considered false.
-
-    If the data is empty, also removes prior errors (such as processing errors)
-    from the field.
-
-    **NOTE** this validator used to be called `Required` but the way it behaved
-    (requiring coerced data, not input data) meant it functioned in a way
-    which was not symmetric to the `Optional` validator and furthermore caused
-    confusion with certain fields which coerced data to 'falsey' values like
-    ``0``, ``Decimal(0)``, ``time(0)`` etc. Unless a very specific reason
-    exists, we recommend using the :class:`InputRequired` instead.
-
-    :param message:
-        Error message to raise in case of a validation error.
-    """
-    field_flags = ('required', )
-
-    def __init__(self, message=None):
-        self.message = message
-
-    def __call__(self, form, field):
-        print('ulaaan')
-        if not field.data or isinstance(field.data, string_types) and not field.data.strip():
-            if self.message is None:
-                message = field.gettext('This field is required.')
-            else:
-                message = self.message
-
-            field.errors[:] = []
-            raise StopValidation(message)
 
 class StepForm(FlaskForm):
-    step = StringField(default="", label="Step", validators=[DataRequired()])
+    step = StringField(label="Step")
+    law = SelectField(label="Law", validators=[InputRequired()], choices=laws)
+    error = None
     options = start_tokens
-    # TODO: Add a length validator in @validators above!
 
 
 class WireForm(Form):
@@ -97,19 +49,25 @@ class WireForm(Form):
 @app.route("/", methods=["GET", "POST"])
 def wire_it():
     form = WireForm(request.form, steps=steps_init)
+    has_error = False
 
     if request.method == "POST":
-        if "next" in request.form:
-            previous_data = form.data
-            form.output = steps2output(steps=previous_data["steps"])
-            # If the output is not well-formatted (i.e. undesirable character in user input),
-            # we do not want to give the next step to the user.
-            if 'wrong' in form.output:
-                pass
+        for step in form.steps:
+            if not step_input_check(step):
+                has_error = True
+                step.error = 'Please fill this step!'
+            elif not step_syntax_check(step):
+                has_error = True
+                step.error = 'Please use correct logic syntax in this step!'
             else:
-                previous_data['steps'].append({"step": "", "csrf_token": ""})
-                form.__init__(data=previous_data)
+                step.error = None
 
+        if has_error:
+            pass
+        elif "next" in request.form:
+            previous_data = form.data
+            previous_data['steps'].append({"step": "", "csrf_token": ""})
+            form.__init__(data=previous_data)
         elif "end" in request.form:
             previous_data = form.data
             form.__init__(data=previous_data)
@@ -121,13 +79,10 @@ def wire_it():
 def steps2output(steps):
     output = []
     for step in steps:
-        step_ = step['step'].strip()
-        if checkSyntax(step_):
-            output.append(step_)
-        else:
-            output.append('This is wrong!')
+        step_, law = step['step'].strip(), step['law']
+        output.append((step_, law))
 
-    return ' | '.join(output)
+    return output
 
 
 if __name__ == "__main__":
