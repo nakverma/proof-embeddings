68441713682d1b4be8cef299a1d1c4eaf5012794
MichelVazirani <michel.vazirani@gmail.com>
2020-12-07 17:58:17 -0500

    investigating memory issues

--- deterministic.py
+++ deterministic.py
@@ -46,7 +46,9 @@ def check_correct_operation(e1, e2, ops, num_ops=1):
         trainer.increment_ops(num_ops)
         trees = trainer.get_trees()
 
-        if len(trees) < 1500:
+        print("len(trees)", len(trees))
+
+        if len(trees) < 1500 and ops[0] != 'Commutativity':
             tree_strs = [t.parse_tree() for t in trees]
             for t in trees:
                 for loc_parses in t.deep_parse_tree():
