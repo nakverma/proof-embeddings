351344b1d29b5dc3789960cf8a5630d35d3e7780
Mehr Kaur <mehrkaur23@gmail.com>
2021-08-11 15:55:21 -0400

    debugged and has tester

--- logic-questioner/a_star_tests.py
+++ logic-questioner/a_star_tests.py
@@ -11,6 +11,8 @@ from create_expressions_mistakes import LogicTreeTrainer
 
 def big_change_favored_weight(n1, n2):
     next = []
+    op_code = 0
+    weight = 0
 
     seed = n1
     trainer = LogicTreeTrainer(seed, expand=None)
@@ -61,10 +63,10 @@ def h3(start, next, ans):
     return abs(len(start)-len(ans)) - abs(len(next)-len(ans))
 
 def h4(start, next, ans):
-    return abs(len(start)-len(ans)) - big_change_favored_weight() * abs(len(next)-len(ans))
+    return abs(len(start)-len(ans)) - big_change_favored_weight(start, next) * abs(len(next)-len(ans))
 
 def h5(start, next, ans):
-    return big_change_favored_weight() * abs(len(next)-len(ans)) / abs(len(start)-len(ans))
+    return big_change_favored_weight(start, next) * abs(len(next)-len(ans)) / abs(len(start)-len(ans))
 
 def h6(start, next, ans):
     return abs(len(start)-len(ans)) - abs(len(next)-len(ans))
@@ -75,15 +77,68 @@ def g1(n1, n2):
 def g2(n1, n2):
     return small_change_favored_weight(n1, n2)
 
-def g3(n1, n2, h):
+def g3(n1, n2,  ans, h):
     CUTOFF = 4 # Needs to be tested and changed
-    if h(n1, n2) > CUTOFF:
+    if h(n1, n2, ans) > CUTOFF:
         return big_change_favored_weight(n1, n2)
     else:
         return small_change_favored_weight(n1, n2)
 
+
+def sample_questions(question_file):
+    with open(question_file) as file:
+        questions = json.load(file)
+    print('Start\tEnd\tCurrent\tNext\tg1\tg2\tg3\th1\th2\th3\th4\th5\th6')
+    with open('output.txt','w') as file:
+        max_col_length = {'start': 25, 'ans': 15, 'current': 25, 'next': 25, 'func': 15}
+        for i, question in enumerate(questions):
+            start = question['question'].split('that ')[1].split(' is')[0]
+            ans = question['answer']
+            steps = question['solution']
+            g_segment_1 = [g1(steps[i], steps[i+1]) for i in range(len(steps)-1)]
+            g_segment_2 = [g2(steps[i], steps[i+1]) for i in range(len(steps)-1)]
+            g_total_1 = [g1(steps[i], steps[i+1]) for i in range(len(steps)-1)]
+            g_total_2 = [g2(steps[i], steps[i+1]) for i in range(len(steps)-1)]
+            for i in reversed(range(len(steps)-2)):
+                g_total_1[i] = g_total_1[i] if i == len(steps)-1 else g_total_1[i] + g_total_1[i+1]
+                g_total_2[i] = g_total_2[i] if i == len(steps)-1 else g_total_2[i] + g_total_2[i+1]
+            for i in range(len(steps)-1):
+                row = start + ' ' * (max_col_length['start']-len(start))
+                row += ans + ' ' * (max_col_length['ans']-len(ans))
+                row += steps[i] + ' ' * (max_col_length['current']-len(steps[i]))
+                row += steps[i+1] + ' ' * (max_col_length['next']-len(steps[i+1]))
+                row += str(g_total_1[i]) + ' ' * (max_col_length['func']-len(str(g_total_1[i])))
+                row += str(g_total_2[i]) + ' ' * (max_col_length['func']-len(str(g_total_2[i])))
+                row += str(h1(start, steps[i+1], ans)) + ' ' * (max_col_length['func']-len(str(h1(start, steps[i+1], ans))))
+                row += str(h2(start, steps[i+1], ans)) + ' ' * (max_col_length['func']-len(str(h2(start, steps[i+1], ans))))
+                row += str(h3(start, steps[i+1], ans)) + ' ' * (max_col_length['func']-len(str(h3(start, steps[i+1], ans))))
+                row += str(h4(start, steps[i+1], ans)) + ' ' * (max_col_length['func']-len(str(h4(start, steps[i+1], ans))))
+                row += str(h5(start, steps[i+1], ans)) + ' ' * (max_col_length['func']-len(str(h5(start, steps[i+1], ans))))
+                row += str(h6(start, steps[i+1], ans)) + ' ' * (max_col_length['func']-len(str(h6(start, steps[i+1], ans))))
+                file.write(row + '\n')
+
 def __main__():
-    pass
+    start = 'qvp'
+    ans = '~p->q'
+    nxt = '~p->q'
+    print("g:")
+    print(g1(start,nxt))
+    print(g2(start,nxt))
+    print(g3(start,nxt, ans, h1))
+    print(g3(start,nxt, ans,h2))
+    print(g3(start,nxt, ans,h3))
+    print(g3(start,nxt, ans,h4))
+    print(g3(start,nxt, ans,h5))
+    print(g3(start,nxt, ans,h6))
+    print("h:")
+    print(h1(start, nxt, ans))
+    print(h2(start, nxt, ans))
+    print(h3(start, nxt, ans))
+    print(h4(start, nxt, ans))
+    print(h5(start, nxt, ans))
+    print(h6(start, nxt, ans))
+    #sample_questions('questions.txt')
+    
 
 if __name__ == '__main__':
     __main__()
